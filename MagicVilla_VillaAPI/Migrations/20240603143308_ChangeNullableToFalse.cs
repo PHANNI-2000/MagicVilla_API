196db76498df2e3380561a4172f16a75c2881661
PHANNITA CHUAYLUEA <phannita.chuayluea@gmail.com>
2024-06-04 12:18:04 +0700

    Section 7 - Consuming API #2

---
+++ MagicVilla_VillaAPI/Migrations/20240603143308_ChangeNullableToFalse.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNullableToFalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 21, 33, 7, 928, DateTimeKind.Local).AddTicks(7553));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 21, 33, 7, 928, DateTimeKind.Local).AddTicks(7569));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 21, 33, 7, 928, DateTimeKind.Local).AddTicks(7572));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 21, 33, 7, 928, DateTimeKind.Local).AddTicks(7573));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 21, 33, 7, 928, DateTimeKind.Local).AddTicks(7575));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 2, 20, 49, 12, 181, DateTimeKind.Local).AddTicks(5852));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 2, 20, 49, 12, 181, DateTimeKind.Local).AddTicks(5866));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 2, 20, 49, 12, 181, DateTimeKind.Local).AddTicks(5868));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 2, 20, 49, 12, 181, DateTimeKind.Local).AddTicks(5870));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 2, 20, 49, 12, 181, DateTimeKind.Local).AddTicks(5873));
+        }
+    }
+}
